7751d7a7c20760806bfefc54770808b07b7fbec1
Praveen Sharma <praveen.sharma@hotwaxsystems.com>
2021-02-26 18:29:58 +0530

    Basic Bean Scope example.

---
+++ src/main/java/main/Config.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+	@Bean
+	public Car getCar() {
+		return new Car();
+	}
+	
+}
